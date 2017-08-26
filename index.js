5b370e47211e47082883daeaab75804a954cc3d3
YosefLm <Yoseflm13@gmail.com>
2017-08-27 00:51:12 +0300

    s

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ lor(arrRandom(), "arrrandom") // בדיקה של הפונקציה הקודמת
 function outputnull() {
     return null;
 }// אאוטטפוט לא קיים, מיועד לנוירונוים עם תשובה "ריקה"
+
 // למלא מערך בנאלים, סיבות באגים\נוחות
 Array.prototype.fillNull = function () { return Array(this.length).fill(null) }
 
@@ -24,16 +25,18 @@ lor(Array(10).fillNull(), "fillnull");
 
 // קלאס דור ראשון
 class firstGeneration {
+    // מקבל מספר אוכלוסיה, אינפוטס אפשריים, אאוטפוטס אפשריים, אפשרי: אובייקט הורה
     constructor(num, inputs, outputs, parent) {
         this.num = num
         this.inputs = inputs;
         this.outputs = outputs;
-        this.basicgen = Array(num).fillNull()
-        this.generation = new Generation(this.num, inputs, outputs, this.basicgen, this.basicgen, true, this)
-        this.parent = parent;
+        this.basicgen = Array((inputs+1)*(outputs+1)).fillNull() // מילוי מערך נאליי של גנים
+        this.generation = new Generation(this.num, inputs, outputs, this.basicgen, this.basicgen, true, this) //יצירת דור
+        this.parent = parent; // הגדרת הורה המשתנה
     }
 } // קלאס ניהול דורות
 class Generation {
+    // מקבל מספר אוכלוסיה, אינפוטים אפשריים, אאוטפוטים אפשריים, בסיס גנטי 1, בסיס גנטי 2, מוטציה:כן\לא, הורה
     constructor(num, inputs, outputs, basicgen, basicgen2, mutation, parent) {
         this.inputs = inputs;
         this.outputs - outputs
@@ -43,23 +46,26 @@ class Generation {
         var t = this;
         // יצירת אוכלוסיה
         this.binots = Array(num).fillNull().map(function (v, i) {
+            // יצירת בינה
             var mybina = new bina(inputs, outputs, basicgen.map(function (v, i) {
-                if (t.mutation)
+                // שילוב שני הבסיסי הגנטים הקודמים, אם מוטציה אז להוסיף מוטציה אפשרית
+                if (t.mutation) 
                     return [basicgen[i] ? basicgen[i] : Math.round(Math.random() * outputs.length), basicgen2[i] ? basicgen2[i] : Math.round(Math.random() * outputs.length), Math.round(Math.random() * outputs.length)].random()
                 else
                     return [basicgen[i] ? basicgen[i] : Math.round(Math.random() * outputs.length), basicgen2[i] ? basicgen2[i] : Math.round(Math.random() * outputs.length)].random()
             }), t)
-            mybina._id = i;
-            mybina._parent = t;
+            mybina._id = i; // לשימוש קלאס דור
+            mybina._parent = t; // כנל
             return mybina;
         });
-        this.parent = parent;
+        this.parent = parent; //הורה
     }
     update(bina) {
+        //עדכון בינה תיאורטית, האם היא חיה או לא
         bina._alive = true;
         bina._death = false;
     }
-    default() {
+    default() { // ברירת מחדל
         return new Generation(10, [0, 1, 2, 3], basicKeys, Array(10).fillNull(), Array(10).fillNull(), true)
     }
 }
@@ -69,19 +75,21 @@ Generation.default = function () {
 }
 //קלאס בינה מלאכותית
 class bina {
+    // נאל של אאוטפוט
     outputnull() {
         return null;
     }
+    // מקבל אינפוטים כמספר או מערך, פלטים גם, גנים אפשרי, הורה
     constructor(inputs, outputs, gen, parent) {
-        if (typeof (inputs) == 'number')
+        if (typeof (inputs) == 'number') // אם מקבלים מספר אינפוטים כמספר
             this.inputs = lor(Array(inputs).fillNull().map(function (v,i) { return arrRandom(); }), "inputs");
-        else if (typeof (inputs) == 'object')
+        else if (typeof (inputs) == 'object') //אם כמערך (אובייקט)
             this.inputs = inputs;
-        if (typeof (outputs) == 'number')
+        if (typeof (outputs) == 'number') // אאוטפוטים אפשריי כמספר
             this.outputs = Array(outputs);
-        else if (typeof (outputs) == 'object')
+        else if (typeof (outputs) == 'object') //אם כמערך (אובייקט)
             this.outputs = outputs;
-        this.gen = gen;
+        this.gen = gen; 
         this.parent = parent;
     }
     // חישוב פיטנס
@@ -92,27 +100,28 @@ class bina {
     geio(input, output) {
         lor(input, "in");
         lor(output, "op");
-        return this.gen[input * output]
+        return this.gen[input * output] //TODO: דורש שינוי כלשהו אני די בטוח 
     }
     // לכאן מכניסים עדכונים של קלט
     update(inputs) {
         //inputs = arr with one of the values [-1,0,1]
         var toreturn = []
-
+        // רץ על האינפוטים שקיבלנו
         for (var index = 0; index < inputs.length; index++) {
             lor(this, "this")
-            var inp = inputs[index] + 1 * lor(this.inputs[index] + 1, "inputs[index]")
-            toreturn.push(lor(this.geio(index, inp), "logio"))
+            //inputs[index] + 1 * lor(this.inputs[index] + 1, "inputs[index]") 
+            var inp = lor(this.inputs[index] + 1, "inputs[index]") // חישוב גן האאוטפוט של האינפוט
+            toreturn.push(lor(this.geio(index, inp), "logio")) // החזרת פלט המתבקש ושמירתו בצד
         }
         lor(toreturn, "toreturn");
         var outputs = this.outputs.map(function (v) { return v });
-        for (var index = 0; index < this.outputs.length; index++) {
-            outputs[index] = toreturn.filter(
+        for (var index = 0; index < this.outputs.length; index++) {// לרוץ על כל הפלטים האפשריים
+            outputs[index] = toreturn.filter( // מיון למחוק ריקים
                 function (v, i) { if (v == index && v != outputnull()) { return true } }
             ).length > 0 ? this.outputs[index] : outputnull();
         }
-        this._parent.update(this, true)
-        return outputs.filter(function (v) { if (v != outputnull()) return true })
+        this._parent.update(this, true)// עדכון ההורה
+        return outputs.filter(function (v) { if (v != outputnull()) return true }) // החזרה אחרי פילטר
     }
 }
 // בדיקה
